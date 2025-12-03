626f488fc0cbe27e9179a575c47ca516f1e397f0
perezmanfredo959@gmail.com <perezmanfredo959@gmail.com>
2025-12-03 13:06:40 -0600

    Portafolio 90% terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Dock, Navbar, Welcome } from "#components"
-import { Terminal, Safari, Resume } from "#windows";
+import { Dock, Home, Navbar, Welcome } from "#components"
+import { Terminal, Safari, Resume, Finder, Text, Image, Contact, Photos } from "#windows";
 import gsap from "gsap";
 
 import { Draggable } from "gsap/Draggable";
@@ -15,6 +15,13 @@ const App = () => {
       <Terminal />
       <Safari />
       <Resume />
+      <Finder />
+      <Text />
+      <Image />
+      <Contact />
+      <Photos />
+
+      <Home />
     </main>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "#components/Navbar"
 import Welcome from "#components/Welcome"
 import Dock from "#components/Dock"
 import WindowsControls from "./WindowsControls"
+import Home from "#components/Home"
 
-export { Navbar, Welcome, Dock, WindowsControls}
+export { Navbar, Welcome, Dock, WindowsControls, Home}
